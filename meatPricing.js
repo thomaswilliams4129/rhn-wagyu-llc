a5ec01e45c9c66c3057fe9584545953dcd41ff4d
Thomas Williams <thwilliams4129@gmail.com>
2022-03-03 17:08:27 -0500

    Completed Home, and Pricing

---
+++ meatPricing.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function () {
+  // FETCHING DATA FROM JSON FILE
+  $.getJSON("meatPricing.json", function (data) {
+    var meat = "";
+
+    // ITERATING THROUGH OBJECTS
+    $.each(data, function (key, value) {
+      //CONSTRUCTION OF ROWS HAVING
+      // DATA FROM JSON OBJECT
+      meat += '<div class="row">';
+      meat += '<div class="col">' + value.Cut + "</div>";
+
+      meat += '<div class="col">' + value.AverageSizePerCut + "</div>";
+
+      meat += '<div class="col">' + value.PricePerLbs + "</div>";
+
+      meat += "</div>";
+    });
+
+    //INSERTING ROWS INTO TABLE
+    $("#tableInfo").append(meat);
+  });
+});
